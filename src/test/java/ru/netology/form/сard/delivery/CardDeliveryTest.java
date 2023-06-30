5c159d565451bdaa8634446bb68a419ce2cee7ee
Yulia <ylianna_sko@mail.ru>
2023-06-30 09:50:27 +0300

    change test

--- src/test/java/ru/netology/form/сard/delivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/form/сard/delivery/CardDeliveryTest.java
@@ -1,33 +1,37 @@
 package ru.netology.form.сard.delivery;
 
 import org.junit.jupiter.api.*;
+
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
+
 public class CardDeliveryTest {
 
-        private String getFutureDate (int daysToAdd){
-            LocalDate currentDate = LocalDate.now();
-            LocalDate futureDate = currentDate.plusDays(daysToAdd);
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-            String formattedDate = futureDate.format(formatter);
-            return formattedDate;
-        }
-        @Test
-        public void shouldSuccessfulRegistrationsCardDelivery () {
-            open("http://localhost:9999/");
-           // SelenideElement form = $("[]");
-            $("[data-test-id=city] input").setValue("Калуга");
-            $(".calendar-input__custom-control input").doubleClick().sendKeys(getFutureDate(3));
-            $("[data-test-id=name] input").setValue("Иванов Иван");
-            $("[data-test-id=phone] input").setValue("+79990001122");
-            $("[data-test-id=agreement]").click();
-            $("button").click();
-            $("[data-test-id=notification] .notification__title").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Успешно!"));
-            $("[data-test-id=notification] .notification__content").shouldHave(exactText("Встреча успешно забронирована на " + getFutureDate(3)));
-
-        }
+    private String getFutureDate(int daysToAdd) {
+        LocalDate currentDate = LocalDate.now();
+        LocalDate futureDate = currentDate.plusDays(daysToAdd);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String formattedDate = futureDate.format(formatter);
+        return formattedDate;
     }
+
+    @Test
+    public void shouldSuccessfulRegistrationsCardDelivery() {
+        open("http://localhost:9999/");
+        // SelenideElement form = $("[]");
+        $("[data-test-id=city] input").setValue("Калуга");
+        $(".calendar-input__custom-control input").doubleClick().sendKeys(getFutureDate(3));
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+79990001122");
+        $("[data-test-id=agreement]").click();
+        $(".button__text").click();
+        $("[data-test-id=notification] .notification__title").shouldHave(exactText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id=notification] .notification__content").shouldHave(exactText("Встреча успешно забронирована на " + getFutureDate(3)));
+
+    }
+}
